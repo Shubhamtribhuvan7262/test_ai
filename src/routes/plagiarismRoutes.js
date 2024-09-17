91c71507ae3e5a372e4cf3f705e4027abe756639
Shubhamtribhuvan7262 <124442690+Shubhamtribhuvan7262@users.noreply.github.com>
2024-09-17 13:35:45 +0530

    Update plagiarismRoutes.js

--- src/routes/plagiarismRoutes.js
+++ src/routes/plagiarismRoutes.js
@@ -8,7 +8,7 @@ const plagiarismController = require("../controllers/plagiarismController");
 
 let storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/");
+    cb(null, "src/uploads/");
   },
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}-${file.originalname}`);
